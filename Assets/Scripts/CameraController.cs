4b703bf955a2174a4c895b65f006827497cf78c0
Tobias Termeczky <tobiastermeczky@gmail.com>
2017-11-08 16:56:54 +0100

    Roll a ball game

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject Player;
+	
+	private Vector3 _offset;
+
+	// Use this for initialization
+	private void Start ()
+	{
+		_offset = transform.position - Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	private void LateUpdate()
+	{
+		transform.position = Player.transform.position + _offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float Speed;
+    public Text CountText;
+    public Text WinText;
+    
+    private Rigidbody _rb;
+    private int _count;
+
+    // Use this for initialization
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _count = 0;
+        SetCountText();
+        WinText.text = "";
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement;
+        switch (Application.platform)
+        {
+            case RuntimePlatform.Android:
+                movement = new Vector3(Input.acceleration.x, 0, -Input.acceleration.z);
+                break;
+            default:
+                movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                break;
+        }
+        _rb.AddForce(movement * Speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            _count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        CountText.text = "Count: " + _count;
+        if (_count >= 12)
+        {
+            WinText.text = "You Win!";
+        }
+    }
+}
